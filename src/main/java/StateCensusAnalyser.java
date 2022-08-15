da8ea36d34b1083ff4fe697fc176f7089e1da2c7
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-15 16:05:49 +0530

    analyse indian states code

--- src/main/java/StateCensusAnalyser.java
+++ src/main/java/StateCensusAnalyser.java
@@ -9,17 +9,10 @@ import java.util.List;
 
 public class StateCensusAnalyser {
     static ArrayList<CSVStateCensus> csvStateCensuses = new ArrayList<>();
+    static ArrayList<CSVState> csvStates = new ArrayList<>();
 
     public int loadData(File file) throws CensusAnalyserException, CsvException, IOException {
-        if (!file.exists()) {
-            throw new CensusAnalyserException("fine not exists");
-        }
-
-        String expectedFileType = "csv";
-        int index = file.toString().lastIndexOf(".");
-        String actualFileType = file.toString().substring(index + 1);
-        if (!expectedFileType.equals(actualFileType)) throw new CensusAnalyserException("file type doesn't match");
-
+        checkInputFile(file);
         FileReader fileReader = new FileReader(file);
         CSVReader csvReader = new CSVReader(fileReader);
 
@@ -32,8 +25,35 @@ public class StateCensusAnalyser {
         for (String[] data : stateCensus) {
             if (data.length < 2) throw new CensusAnalyserException("Incorrect Delimiter");
             csvStateCensuses.add(new CSVStateCensus((data[0]), Long.parseLong(data[1].replace(",", ""))));
-
         }
         return csvStateCensuses.size();
+    }
+
+    public int loadStates(File file) throws CensusAnalyserException, IOException, CsvException {
+        checkInputFile(file);
+        FileReader fileReader = new FileReader(file);
+        CSVReader csvReader = new CSVReader(fileReader);
+
+        List<String[]> states = csvReader.readAll();
+        String[] header = states.get(0);
+        if (!header[1].equals("State/ UT") || !header[2].equals("Abbreviation"))
+            throw new CensusAnalyserException("header mismatch");
+
+        states.remove(header);
+        for (String[] data : states) {
+            if (data.length < 2) throw new CensusAnalyserException("Incorrect Delimiter");
+            csvStates.add(new CSVState(data[1], data[2]));
+        }
+        return csvStates.size();
+    }
+
+    void checkInputFile(File file) throws CensusAnalyserException {
+        if (!file.exists()) {
+            throw new CensusAnalyserException("fine not exists");
+        }
+        String expectedFileType = "csv";
+        int index = file.toString().lastIndexOf(".");
+        String actualFileType = file.toString().substring(index + 1);
+        if (!expectedFileType.equals(actualFileType)) throw new CensusAnalyserException("file type doesn't match");
     }
 }
